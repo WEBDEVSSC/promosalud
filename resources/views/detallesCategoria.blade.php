ea0d8c464b23536e32b5a37d20e6069e11d29ff9
Cesar Torres Desarrollo <soportewebcoah@gmail.com>
2025-05-07 12:57:16 -0600

    HEADER Y BREADCUM

--- resources/views/detallesCategoria.blade.php
+++ resources/views/detallesCategoria.blade.php
@@ -13,6 +13,27 @@
 </head>
 <body>
 
+      <!-- Bootstrap 5 CDN (asegúrate de incluirlo en tu <head>) -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Secretaría de Salud de Coahuila de Zaragoza</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+      aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Bootstrap JS (al final del body) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <!-- Contenido de la página -->
     <div class="container">
 
@@ -48,6 +69,13 @@
                 <iframe width="860" height="615" src="https://www.youtube.com/embed/DEWHgwDTiCU?si=s-O3uTtvBA0zu2RV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </center>
         @endif
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ route('welcome') }}">Inicio</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Página actual</li>
+            </ol>
+          </nav>
 
         <div class="row">
             <div class="col-md-12">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,28 @@
 </head>
 <body>
 
+  <!-- Bootstrap 5 CDN (asegúrate de incluirlo en tu <head>) -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Secretaría de Salud de Coahuila de Zaragoza</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+      aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Bootstrap JS (al final del body) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
     <!-- Contenido de la página -->
     <div class="container">
 
